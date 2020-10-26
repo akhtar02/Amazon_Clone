3c887a5ce4a40c53e0c895162cd8fdd409728ca4
akhtar hussain <akhtarhussain0788@gmail.com>
2020-10-27 01:46:24 +0500

    Amazon Clone use Reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+    <Switch>
+      <Route path = "/Checkout">
+        <h1>CheckOut</h1>
+      </Route>
+      <Route path = "/Login">
+        <h1>Login</h1>
+      </Route>
+      <Route path="/">
+        <h1>Home Page !!</h1>
+      </Route>
+     
+    </Switch>
+      </div>
+    </Router>
+    
+
+
+    //  <h1>Let's build an Amazon Clone with React Js</h1>
+      // {/* We Need REact-Router */}
+
+      // {/* localhost.com */}
+      // {/* localhost.com/checkout */}
+      // {/* localhost.com/login */}
+
+   
   );
 }
 
